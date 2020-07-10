6e19fdc271786f31e6fdff1c93955eb970188863
LONGA Valerie <longaval@gm0winl136.bureau.si.interne>
2020-07-10 13:35:44 +0200

    [OC-831] Update getting started with routing mechanism 3

--- client/example3/deleteCard.sh
+++ client/example3/deleteCard.sh
@@ -1,1 +1,1 @@
-curl -s -X DELETE http://localhost:2102/cards/criticalSituation_alert1 -H "Content-type:application/json"
+curl -s -X DELETE http://localhost:2102/cards/criticalSituation.alert1 -H "Content-type:application/json"
